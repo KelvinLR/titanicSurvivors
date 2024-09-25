e29f784ea3b6acb56a75b4acfae8ba0e61a3e98a
KelvinLR <kelvindelima710@gmail>
2024-09-24 21:37:21 -0300

    feat: create CustomButton component

--- lib/components/custom_button.dart
+++ lib/components/custom_button.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:titanic_app/theme/design_system.dart';
 
 class CustomButton extends StatelessWidget {
   const CustomButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double contextWidth = MediaQuery.sizeOf(context).width;
+    double contextHeight = MediaQuery.sizeOf(context).height;
+    return SizedBox(
+      width: contextWidth * (283 / 360),
+      height: contextHeight * (59 / 800),
+      child: Material(
+        color: const Color(0xffB6FF89),
+        borderRadius: BorderRadius.circular(10),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(10),
+          onTap: () {},
+          child: Center(
+            child: Text(
+              "Prever Sobrevivência",
+              style: TitanicAppFonts.formTextStyle,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
