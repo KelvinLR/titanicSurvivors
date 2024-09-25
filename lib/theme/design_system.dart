856bb65a2066b51bfcbe05ba0b0784e8863190d3
KelvinLR <kelvindelima710@gmail>
2024-09-24 21:36:48 -0300

    feat: create designsystem to project and create custom text styles

---
+++ lib/theme/design_system.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TitanicAppFonts {
+  static TextStyle formTextStyle = const TextStyle(
+    fontFamily: "Quicksand",
+    fontWeight: FontWeight.bold,
+    fontSize: 14,
+  );
+  static TextStyle bodyTextStyle = const TextStyle(
+    fontFamily: "Quicksand",
+    fontWeight: FontWeight.normal,
+    fontSize: 14,
+  );
+  static TextStyle appBarTextStyle = const TextStyle(
+    fontFamily: "Quicksand",
+    fontWeight: FontWeight.bold,
+    fontSize: 14,
+  );
+}
